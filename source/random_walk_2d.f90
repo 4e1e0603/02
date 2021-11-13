315d0202525d11c1a292b89104fec3c1896bdc2a
David Landa <david.landa@protonmail.com>
2021-11-13 19:04:30 +0100

    Move coordinate generator to subroutine

--- source/random_walk_2d.f90
+++ source/random_walk_2d.f90
@@ -12,6 +12,11 @@
 ! 2. Random walk without immediate returns
 ! steps in three possible directions with the same probability, the first step is in any direction
 !
+! Refences:
+! http://utf.mff.cuni.cz/vyuka/NTMF021/homeworks/HW_task_1.pdf
+
+! Plot the mean distance vs number of steps (+ error bars given by standard deviation)
+! Check that for every random walks dependence is of the form $R(n) = c n^{\alpha}$
 
 
 module random_walk
@@ -24,43 +29,48 @@ module random_walk
 
     contains
 
-    ! Realise of 2-dimensional dicrete random walk for the given number of steps.
+    subroutine pick_coordinate(c, r)
+        implicit none
+
+        real, intent(in) :: r
+        real, intent(inout) :: c
+
+        if (r >= 0.5) then
+            c = c + 1
+        else
+            c = c - 1
+        end if
+
+    end subroutine
+
+    ! Realize 2-dimensional dicrete random walk for the given number of steps.
     !
     ! We call this random walk as simple because thre are no restrictions on the step to
     ! be taken.
     !
     function simple_random_walk_2D(steps) result(walk)
-
         implicit none
 
         integer, intent(in) :: steps                  ! The input variable.
         real, dimension(:,:), allocatable :: walk     ! The output variable.
 
         real :: rvalue                                ! The random value.
-        real :: x = 0, y = 0                          ! The starting position.
+        real :: x = 0.0,  y = 0.0                     ! The starting position.
         integer :: step                               ! A current step.
 
         allocate(walk(1:3,1:steps))                   ! Set the size of the output array.
                                                       ! We store coordinates as [x, y, ..., x, y].
         do step = 1, steps
 
-            call random_number(rvalue)                 ! The `x` coordinate direction choice.
-            if (rvalue >= 0.5) then
-                x = x + 1
-            else
-                x = x -1
-            end if
+            call random_number(rvalue)                ! The `x` coordinate direction choice.
+            call pick_coordinate(x, rvalue)
 
-            call random_number(rvalue)                 ! The `y` coordinate direction choice.
-            if (rvalue >= 0.5) then
-                y = y + 1
-            else
-                y = y - 1
-            end if
+            call random_number(rvalue)                ! The `y` coordinate direction choice.
+            call pick_coordinate(y, rvalue)
 
-            walk(1, step) = x                         ! Save the `(x, y, d)` coordinates and distance.
-            walk(2, step) = y
-            walk(3, step) = sqrt(real(x * x) + y*y)   ! Euclidean distance.
+            walk(1, step) = x                         ! Save the `(x, y, d)` coordinates and distance
+            walk(2, step) = y                         ! and calculate Euclidean distance
+            walk(3, step) = sqrt(real(x * x) + y*y)
         end do
 
     end function
