867e75b8f954cc9d414e5b04f980f028a1654f65
David Landa <david.landa@protonmail.com>
2021-11-15 20:52:35 +0100

    Refactor random walk example to pure functions and debug (there is some error)

--- source/random_walk/random_walk_2d.f90
+++ source/random_walk/random_walk_2d.f90
@@ -15,104 +15,173 @@
 !        steps in three possible directions with the same probability, the first step is in any direction
 !
 ! REFERENCE:
-!   http://utf.mff.cuni.cz/vyuka/NTMF021/homeworks/HW_task_1.pdf
+! - http://utf.mff.cuni.cz/vyuka/NTMF021/homeworks/HW_task_1.pdf
+! - http://compphysics.github.io/ComputationalPhysics/doc/pub/rw/html/._rw-bs061.html
+! - https://github.com/jakehanson/2D_RANDOM_WALK/blob/master/main.cpp
+! - https://balitsky.com/teaching/phys420/Ph4_random.pdf
+! - https://blogs.sas.com/content/iml/2015/07/08/drunkards-walk.html
+! - https://blogs.sas.com/content/iml/2015/08/12/2d-drunkards-walk.html
+! - https://gist.github.com/rsnemmen/879cb5452effcf2d20a8f0041b8724e4
+! - https://cyber.dabamos.de/programming/modernfortran/random-numbers.html
 !
-module random_walk
-
-implicit none
-
-private
-
-public :: simple_random_walk_2D
-
-contains
-
-pure function evolve_coordinate(random, current) result(next)
-    !! Evolve the coordinate value based on the given random value.
-    !! (This is a helper procedure.)
-    implicit none
-
-    real, intent(in) :: random  ! The random number between [0,1].
-    real, intent(in) :: current ! The current coordinate value.
-    real :: next
-
-    ! Choose direction and update coordinate.
-    if (random >= 0.5) then
-        next = current + 1
-    else
-        next = current - 1
-    end if
-
-end function
-
-function simple_random_walk_2D(steps, trials) result(walks)
-    !! Realize 2-dimensional simple random walk on ℤ for the given number of steps.
-    !! Computes the `x`, `y` coordinates and Euclidean distance from the origin.
-
-    implicit none
-
-    integer, intent(in) :: steps, trials            ! The input variable.
-    real, dimension(:,:,:), allocatable :: walks    ! The output array for (x,y,d).
-
-    real :: rvalue                                  ! The random value.
-    real :: x, y                                    ! The coordinates.
-    integer :: step, trial                          ! A current step.
-
-    allocate(walks(1:trials, 1:steps, 1:3))         ! Allocate the output array.
-
-    do trial = 1, trials
-
-        x = 0.0                                     ! Start always at origin.
-        y = 0.0
-
-        do step = 1, steps
-
-            call random_number(rvalue)
-            x = evolve_coordinate(rvalue, x)
-
-            call random_number(rvalue)
-            y = evolve_coordinate(rvalue, y)
-
-            ! Save the coordinates and Euclidean distance
-            walks(trial, step, 1) = x
-            walks(trial, step, 2) = y
-            walks(trial, step, 3) = sqrt(x ** 2 + y ** 2)
-
-        end do
-    end do
-
-end function
-
-! function restricted_random_walk_2D(steps, trials) result() ... end function
-
-end module
-
+! DECISIONS:
+! ---
+! TODO I decided to pass a random numbers as values to functions.
+! The reason is that it is more testable the generatei inside functions.
+! We can pregenreate random values
+! at the beginning of the program and then pass them down to procedures/fucntions.
+! ---
+! I also decided to use type (structure) `position`.
+! The reason is that it better express a domain model and encapsultes `x`, `y` coordinates.
+! ---
+! Can  we determined trials and steps from random?
 !
+!
+! TODO function persistent_random_walk_2D(steps, trials) result() ... end function
+! TODO function restricted_random_walk_2D(steps, trials) result() ... end function
 ! TODO Simple command line interface.
 ! HOWTO Write an array to CSV like format?
 !
+module random_walk
+
+    implicit none
+
+    private
+
+        !! The position on 2-dimensional lattice.
+        type, public :: position
+            real :: x = 0.0;
+            real :: y = 0.0;
+        end type
+
+    public :: simple_random_walk_2D, update_position
+
+    contains
+
+    pure function update_position(random, current) result(updated)
+        !! Update the current position.
+        !!
+        !!            N
+        !!          W o E
+        !!            S
+        !!
+        !! @param random  The random number.
+        !! @param current The current position.
+        !! @return output The updated position.
+        !!
+        implicit none
+
+        real,           intent(in)  :: random
+        type(position), intent(in)  :: current
+        type(position)              :: updated
+
+        updated % x = current % x
+        updated % y = current % y
+
+        if (random >= 0.0 .and. random < 0.25) then
+            updated % x = (current % x) - 1 ! W
+        end if
+
+        if (random >= 0.25 .and. random < 0.5) then
+            updated % x = (current % x) + 1 ! E
+        end if
+
+        if (random >= 0.5 .and. random < 0.75) then
+            updated % y = (current % y) - 1 ! S
+        end if
+
+        if (random >= 0.75 .and. random <= 1.0) then
+            updated % y = (current % y) + 1 ! E
+        end if
+
+
+    end function
+
+
+    pure function simple_random_walk_2D(steps, trials, randoms) result(walks)
+        !! Realize 2-dimensional simple random walk on ℤ for the given number of steps.
+        !!
+        !! @param steps
+        !! @param trials
+        !! @param randoms
+        !! @return  The  `x`, `y` position coordinates and Euclidean distance from the origin.
+        !!
+        implicit none
+
+        integer, intent(in) :: steps, trials            ! The input variable.
+        real, dimension(:,:,:), allocatable :: walks    ! The output array for (x,y,d).
+
+        real, intent(in)    :: randoms(trials, steps)   ! The random numbers.
+        type(position)      :: current                  ! The current position (0,0).
+        integer             :: trial, step              ! The current trial and step.
+
+        allocate(walks(1:trials, 1:steps, 1:3))         ! Allocate the output array.
+
+        do trial = 1, trials
+            ! Reset the position before each run.
+            current % x = 0.0
+            current % y = 0.0
+
+            do step = 1, steps
+                current = update_position(randoms(trial, step), current)
+                ! Save the position and distance.
+                walks(trial, step, 1) = current % x
+                walks(trial, step, 2) = current % y
+                walks(trial, step, 3) = sqrt((current % x) ** 2 + (current % y) ** 2)
+            end do
+        end do
+
+    end function
+
+end module
+
+
 program random_walk_simulation
 
-    use random_walk, only: simple_random_walk_2D
+    use random_walk, only: simple_random_walk_2D, position, update_position
 
     use, intrinsic :: iso_fortran_env, only: sp=>real32, dp=>real64
 
     implicit none
 
     integer :: step, trial
-    integer, parameter :: STEPS = 5, TRIALS = 2
+    integer, parameter :: STEPS = 10, TRIALS = 1
 
-    real :: walk(1:TRIALS, 1:STEPS, 1:3)
+    type(position) :: current, updated
+
+    real :: walks(1:TRIALS, 1:STEPS, 1:3)
+    real :: randoms(1:TRIALS, 1:STEPS)
 
     ! Compute the random walk simulation.
-    walk = simple_random_walk_2D(TRIALS, STEPS)
+    call random_number(randoms)
+    ! walks = simple_random_walk_2D(TRIALS, STEPS, randoms)
+
+    ! TESTS
+    ! GIVEN We are on postion 0,0, WHEN the rundom number is < 0.5 THEN we should move in `x` direction.
+    print *, "CURRENT", current
+    print *, "UPDATED",  updated
+
+    updated = update_position(randoms(1, 1), current)
+
+    print *, "RANDOM", randoms(1, 1)
+
+    if (randoms(1, 1) < 0.5) then
+        print *, "SHOULD GO X", (updated % x) != 0.0
+    end if
+    print *, "UPDATED", updated
+
 
     ! Write the result to the standard output.
-    do trial = 1, TRIALS
-        do step = 1, STEPS
-            write(*,'(f0.1,a,f0.1,a,f0.4)') walk(trial, step, 1), '|',  walk(trial, step, 2), '|', walk(trial, step, 3)
-        end do
-        write(*, *) ''
-    end do
+    ! do trial = 1, TRIALS
+    !     do step = 1, STEPS
+    !         write(*,'(f0.1,a,f0.1,a,f0.4)')  &
+    !              walks(trial, step, 1), '|', &
+    !              walks(trial, step, 2), '|', &
+    !              walks(trial, step, 3)
+    !     end do
+    !     write(*, *) ''
+    ! end do
+
+
 
 end program
